62d45da4ece75333e13cac419a489196496d9edb
Brian Neeland <brian@neeland.org>
2025-12-03 20:53:13 -0700

    Format

--- App.tsx
+++ App.tsx
@@ -6,49 +6,49 @@ import './global.css';
 import { auth } from 'lib/auth';
 
 export default function App() {
-  const [email, setEmail] = useState("");
-  const [otp, setOtp] = useState("");
+  const [email, setEmail] = useState('');
+  const [otp, setOtp] = useState('');
 
   return (
     <>
       <ScreenContent title="Home" path="App.tsx">
         <Text>Get OTP</Text>
-      <TextInput
-        value={email}
-        onChangeText={(value) => {
-          setEmail(value);
-        }}
-      />
-      <Button
-        title="Submit"
-        onPress={async () => {
-          const response = await auth.emailOtp.sendVerificationOtp({
-            email, // required
-            type: "sign-in", // required
-          });
-          console.log("response");
-          console.log(response);
-        }}
-      />
-      <Text>Verify OTP</Text>
-      <TextInput
-        value={otp}
-        onChangeText={(value) => {
-          setOtp(value);
-        }}
-        keyboardType="numeric"
-      />
-      <Button
-        title="Submit"
-        onPress={async () => {
-          const response = await auth.signIn.emailOtp({
-            email, // required
-            otp, // required
-          });
-          console.log("response");
-          console.log(response);
-        }}
-      />
+        <TextInput
+          value={email}
+          onChangeText={(value) => {
+            setEmail(value);
+          }}
+        />
+        <Button
+          title="Submit"
+          onPress={async () => {
+            const response = await auth.emailOtp.sendVerificationOtp({
+              email, // required
+              type: 'sign-in', // required
+            });
+            console.log('response');
+            console.log(response);
+          }}
+        />
+        <Text>Verify OTP</Text>
+        <TextInput
+          value={otp}
+          onChangeText={(value) => {
+            setOtp(value);
+          }}
+          keyboardType="numeric"
+        />
+        <Button
+          title="Submit"
+          onPress={async () => {
+            const response = await auth.signIn.emailOtp({
+              email, // required
+              otp, // required
+            });
+            console.log('response');
+            console.log(response);
+          }}
+        />
       </ScreenContent>
       <StatusBar style="auto" />
     </>
--- lib/auth.ts
+++ lib/auth.ts
@@ -1,16 +1,16 @@
-import { expoClient } from "@better-auth/expo/client";
-import { emailOTPClient } from "better-auth/client/plugins";
-import { createAuthClient } from "better-auth/react";
-import * as SecureStore from "expo-secure-store";
+import { expoClient } from '@better-auth/expo/client';
+import { emailOTPClient } from 'better-auth/client/plugins';
+import { createAuthClient } from 'better-auth/react';
+import * as SecureStore from 'expo-secure-store';
 
 export const auth = createAuthClient({
-  baseURL: "https://local.uptrack.dev", // Base URL of your Better Auth backend.
+  baseURL: 'https://local.uptrack.dev', // Base URL of your Better Auth backend.
   // baseURL: "http://localhost:3000", // Base URL of your Better Auth backend.
   plugins: [
     emailOTPClient(),
     expoClient({
-      scheme: "mobile",
-      storagePrefix: "mobile",
+      scheme: 'mobile',
+      storagePrefix: 'mobile',
       storage: SecureStore,
     }),
   ],
