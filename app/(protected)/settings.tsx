29b170c699cf8dd33ade799e2e98ffbf3aada1ee
Brian Neeland <brian@neeland.org>
2025-12-24 14:59:10 -0700

    Format minimal settings placeholder page

--- app/(protected)/settings.tsx
+++ app/(protected)/settings.tsx
@@ -1,10 +1,14 @@
+import Link from "components/Link";
 import Text from "components/Text";
 import { View } from "react-native";
 
 export default function Settings() {
   return (
-    <View>
-      <Text>settings</Text>
+    <View className="gap-5 p-5">
+      <Text>Go to emberline.app to access your settings:</Text>
+      <Link href="https://www.emberline.app/settings/checkins">
+        <Text>Settings on emberline.app</Text>
+      </Link>
     </View>
   );
 }
